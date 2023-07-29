b3d774c7ff2b21f7b3cce917627a1d679437a0d0
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-07-29 11:34:41 +0600

    update multiplication table

--- multiplicationTable.js
+++ multiplicationTable.js
@@ -1,6 +1,8 @@
 function multiplicationTable(num) {
+    console.log(`Generate Multiplication Table for: ${num}`);
     for (let i = 1; i <= 10; i++) {
       const result = num * i;
+      
       console.log(`${i} X ${num} = ${result}`);
     }
   }
